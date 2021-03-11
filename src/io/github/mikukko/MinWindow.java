8cb5ae6661c9628cafb8e95d254d00461b567641
mikukko <im.ifuo@outlook.com>
2021-03-11 15:15:25 +0800

    76修改

--- src/io/github/mikukko/MinWindow.java
+++ src/io/github/mikukko/MinWindow.java
@@ -1,12 +1,21 @@
 package io.github.mikukko;
 
+import java.util.HashMap;
+
 /**
  * @author Mikukko
  * @date 2021/3/10
  */
 public class MinWindow {
-    public static void main(String[] args) {
+    HashMap<Character, Integer> sMap = new HashMap<>();
+    HashMap<Character, Integer> tMap = new HashMap<>();
 
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANCABCDD";
+        String t = "ABC";
+        new MinWindow().minWindow(s, t);
+
+        //System.out.println();
     }
 
     /**
@@ -19,26 +28,31 @@ public class MinWindow {
      * 输出："BANC"
      */
     public String minWindow(String s, String t) {
-        int left = 0, right = 0;
-        int max = 0;
-        int count = t.length();
-        for (int i = 0; ; ) {
-            if (left == count - 1) {
-                break;
-            }
-            if (count <= 0) {
-                max = Math.max(max, right - left + 1);
-                left += 1;
-                count = t.length();
-            }
-            String s1 = String.valueOf(s.charAt(i));
-
-            if (t.contains(s1)) {
-                t = t.replaceAll(s1, "");
-            }
-            right += 1;
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
 
-        return "";
+        int left = 0, right = 0;
+        int length = s.length();
+        String sub = "";
+
+        while (right < length) {
+            //构建滑动窗口
+            right++;
+
+            sub = s.substring(left, right);
+            for (int i = 0; i < sub.length(); i++) {
+                if (sMap.containsKey(sub.charAt(i))) {
+                    sMap.put(sub.charAt(i), sMap.get(s.charAt(i)) + 1);
+                } else {
+                    sMap.put(sub.charAt(i), 0);
+                }
+            }
+            //如果窗口中的元素包含t中的所有元素, 使用哈希表来判断是否包含字符
+
+        }
+
+        return sub;
     }
 }
