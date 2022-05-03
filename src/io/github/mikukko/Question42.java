639e97dd123dacc876167122383f9ff793776be8
mikukko <im.ifuo@outlook.com>
2022-05-03 21:23:58 +0800

    TODO

---
+++ src/io/github/mikukko/Question42.java
@@ -0,0 +1,16 @@
+package io.github.mikukko;
+
+public class Question42 {
+
+
+    /**
+     * TODO
+     *
+     * 42. 接雨水
+     *
+     */
+    public int trap(int[] height) {
+
+        return 0;
+    }
+}
