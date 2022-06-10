8d7fc932b86f8a8734d6ccc2fddb0eaf777a89b6
mikukko <im.ifuo@outlook.com>
2022-06-10 11:31:30 +0800

    21. 合并两个有序链表

---
+++ src/io/github/mikukko/Question21.java
@@ -0,0 +1,50 @@
+package io.github.mikukko;
+
+public class Question21 {
+
+
+    /*
+     * 21. 合并两个有序链表
+     *
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     * 输入：l1 = [1,2,4], l2 = [1,3,4]
+     * 输出：[1,1,2,3,4,4]
+     *
+     * 递归法
+     * 将两个链表头部值较小的一个节点与剩下元素的 merge 操作结果合并
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
